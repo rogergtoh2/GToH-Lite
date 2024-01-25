0ad3ed7200990a6be897212352a25a7bba43fc5d
rogergtoh <157563838+rogergtoh@users.noreply.github.com>
2024-01-24 22:39:05 -0800

    GRASS LEVEL EDITOR

--- scripts/levelCreator.js
+++ scripts/levelCreator.js
@@ -85,7 +85,8 @@ var blockTypeButtons = [
   ['yellowblock', 'win'],
   ['portalgreen', 'tp', {tags: [0, 0]}],
   ['rgravblock', 'greverse'],
-  ['vines', 'vines']
+  ['vines', 'vines'],
+  ['grass', 'grass']
 ];
 var sideBarOptions = {
   key: [
