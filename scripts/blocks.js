ab7275685d9bfa041425fc06348a643e5c2f33b7
rogergtoh <157563838+rogergtoh@users.noreply.github.com>
2024-01-24 22:41:34 -0800

    PLEASE WORK

--- scripts/blocks.js
+++ scripts/blocks.js
@@ -56,8 +56,6 @@ class Block {
         break;
       case 'fullgrass':
       case 'grass':
-        this.img = cliDir + 'textures/grassblock.png';
-        break;
       case 'dirt':
       this.img = cliDir + 'textures/grassblock.png';
       case 'block':
