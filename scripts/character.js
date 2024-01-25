150037f174694db282469463a7be7b4c15bacff9
rogergtoh <157563838+rogergtoh@users.noreply.github.com>
2024-01-24 22:45:02 -0800

    fix

--- scripts/character.js
+++ scripts/character.js
@@ -143,8 +143,6 @@ class Character {
               return;
             case 'vines':
               break;
-            case 'dirt'
-              return true;
             case 'decor':
               if(i.tags[1] === false && i.tags.includes('moderatorTest')) {
                 i.tags[1] = true;
