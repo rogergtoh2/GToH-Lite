cadfdd706f2a989236936b7433c419ec1a5b83a9
rogergtoh <157563838+rogergtoh@users.noreply.github.com>
2024-01-25 18:33:04 -0800

    vines fix

--- scripts/levelCreator.js
+++ scripts/levelCreator.js
@@ -85,7 +85,7 @@ var blockTypeButtons = [
   ['yellowblock', 'win'],
   ['portalgreen', 'tp', {tags: [0, 0]}],
   ['rgravblock', 'greverse'],
-  ['vines', 'vines']
+  ['vine', 'vines']
 ];
 var sideBarOptions = {
   key: [
