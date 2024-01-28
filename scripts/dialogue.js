b9b83c81867f90196efea23684b8602143851ca8
rogergtoh <157563838+rogergtoh@users.noreply.github.com>
2024-01-27 18:33:40 -0800

    lol

--- scripts/dialogue.js
+++ scripts/dialogue.js
@@ -1,35 +1,5 @@
 const dialogueEnter = [
-'Ya got money?',
-'My wife left me.',
-'I`m so alone.',
-'I got hats!',
-'Whaddya Want?',
-'I know you\'re playing this in class.',
-'Get sum stuff!',
-'He\'s watching.',
-'¿Papa frita?',
-"Sup, scrub?",
-"Not this guy/girl/thing again.",
-"Not this thing again.",
-"Whats the point of bulding forts we goin night night",
-"Shoo.",
-"Enviar fotos de los pies?",
-"Oh! I-I didn't see you there!",
-"Money pays the bills. So pay me too.",
-"Yes, father?",
-"bopeebo.",
-"Lookin' for some b a s e d stuff?",
-"Save them.",
-"Henya?",
-'A little busy right now.',
-"Just in time! I'm watching the Fortnite World Cup!",
-"It's all a game.",
-"Give me you.",
-"Here to discuss FNaF lore?",
-"I miss my kids.",
-"Hide baby peach.",
-"Hee hee hee ha",
-"50% off on the @8^!@# in back."
+"Gone fishing!"
 ];
 const dialogueBought = [
   "+5 social credit",
@@ -46,26 +16,7 @@ const dialogueBought = [
   "Tra La La, the 'c' button actaully does something."
 ];
 const dialogueLeave = [
-'Yeah whatever, bye.',
-'I never liked you anyway.',
-'Bruh moment.',
-'See you later.',
-"You'll be back.",
-"D:",
-"*Waves cutely awaiting your return",
-"#ratio",
-"Adventure awaits, scrub.",
-"Now back to watching Pokimane.",
-"Am I scary yet?",
-"They didn't even say goodbye...",
-"Sorry we couldn't skype tonight, but that's alright.",
-"Bye bozo.",
-"Zad...",
-"And bid thee farewell.",
-"Fine, get bent.",
-"bruh.",
-"God speed!",
-"qwerty."
+'Be back soon!"
 ];
 
 function OpenDialogue(txt = null) {
