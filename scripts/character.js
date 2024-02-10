548c6dcf9b07bc689259b4cf5753e4789d9ff55d
rogergtoh <157563838+rogergtoh@users.noreply.github.com>
2024-02-10 15:23:53 -0800

    ???????

--- scripts/character.js
+++ scripts/character.js
@@ -124,6 +124,13 @@ class Character {
                 statuses.push('icy');
               prevTouchIcy = true;
               return true;
+            case 'water':
+              if (!statuses.includes('icy'))
+                statuses.push('icy');
+              prevTouchIcy = true;
+              if (!statuses.includes('mud')) {
+                statuses.push('mud');
+              break;
             case 'mud':
               if (!statuses.includes('mud')) {
                 statuses.push('mud');
