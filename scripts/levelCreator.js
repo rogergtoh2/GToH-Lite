4984cf3b36ccdcd09f7517c75d6ea2c9b2f58675
rogergtoh <157563838+rogergtoh@users.noreply.github.com>
2024-03-29 21:10:49 -0700

    lowgrav attempt 0

--- scripts/levelCreator.js
+++ scripts/levelCreator.js
@@ -87,7 +87,8 @@ var blockTypeButtons = [
   ['rgravblock', 'greverse'],
   ['vines', 'vine'],
   ['textblock', 'text', {tags: ["test", 30]}],
-  ['water', 'water']
+  ['water', 'water'],
+  ['lowgravblock', 'lowgravblock']
 ];
 var sideBarOptions = {
   key: [
